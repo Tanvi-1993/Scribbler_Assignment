309f04960dee4c1f9cfe75e77954d5581270333f
Tanvi-1993 <random@example.com>
2021-03-08 22:42:01 +0530

    Final Commit

---
+++ js/bloglist.js
@@ -0,0 +1,9 @@
+var modal = document.getElementById('deletePostModal');
+
+function openModal(){
+    modal.style.display = 'block';
+}
+
+function closeModal(){
+    modal.style.display = 'none';
+}
